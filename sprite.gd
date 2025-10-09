e6c0b7cd59ff6bc83367021b5aff7716b851c656
Aidan Kametz <73295047+SilverAidan@users.noreply.github.com>
2025-10-08 20:22:40 -0400

    testing

--- sprite.gd
+++ sprite.gd
@@ -1,1 +1,10 @@
 extends Sprite2D
+var speed = 500
+
+func _process(delta):
+	var direction = Vector2.ZERO
+	direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	if direction != Vector2.ZERO:
+		direction = direction.normalized()
+	
+	position += direction * speed * delta
