e3cba03f28b041df3924b0fc44e0e9a35c6fabe0
Aidan Kametz <73295047+SilverAidan@users.noreply.github.com>
2025-10-22 17:52:01 -0400

    added all character tres files

--- Resources/NpcData.gd
+++ Resources/NpcData.gd
@@ -1,5 +1,6 @@
 class_name NpcData extends Resource
 
 @export_file("*.json") var dialogue_file_path: String
+@export_file("*.json") var order_file_path: String
 @export var expressions: Dictionary[String, Texture2D]
 @export var default_expression_id: String
