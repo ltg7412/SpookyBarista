010e5df4b287a569c5914d6cbf637541316fca48
MiningCow <40306125+MiningCow@users.noreply.github.com>
2026-04-07 20:23:09 -0400

    Add sounds

--- Things/CupStack/cup_stack.gd
+++ Things/CupStack/cup_stack.gd
@@ -4,7 +4,10 @@ class_name CupStack extends Node2D
 @export var cup_offset: int = 50
 @export var cup_scene: PackedScene
 @export var tool_container: ToolContainer
+@onready var pickup_sound: AudioStreamPlayer = $PickupSound
+@onready var put_down_sound: AudioStreamPlayer = $PutDownSound
 @onready var cup_cushion = $CupCushion
+
 
 func _ready() -> void:
 	cup_cushion.cup_offset = Vector2(0, -cup_offset)
@@ -19,4 +22,9 @@ func create_cups(count: int) -> void:
 				cup.pin_component.force_pin(cup_cushion)
 		)
 		tool_container.add_child(cup)
-		
+
+func _on_cup_cushion_unpinned(_pin: PinComponent) -> void:
+	pickup_sound.play()
+
+func _on_cup_cushion_pinned_by(_pin: PinComponent) -> void:
+	put_down_sound.play()
